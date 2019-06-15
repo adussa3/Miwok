e4ce1d301528ff5dc35c459f7710c4b94fa79271
Anunoy Dussa <adussa3@gmail.com>
2019-06-15 15:31:03 -0400

    Added hasImage function to Word class

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -3,35 +3,69 @@ package com.example.android.miwok;
 public class Word {
 
     // Default translation for the word
-    private String defaltTranslation;
+    private String mDefaultTranslation;
 
     // Miwok translation for the word
-    private String miwokTranslation;
+    private String mMiwokTranslation;
+
+    // Image resource ID for the word
+    private int mImageResourceId;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     /**
      * Constructs a new Word with initial values of the default language and Miwok language
      *
-     * @param defaltTranslation is the word in a language that the user is already familiar with
+     * @param defaultTranslation is the word in a language that the user is already familiar with
      * @param miwokTranslation is the word in the Miwok language
      */
-    public Word(String defaltTranslation, String miwokTranslation) {
-        this.defaltTranslation = defaltTranslation;
-        this.miwokTranslation = miwokTranslation;
+    public Word(String defaultTranslation, String miwokTranslation) {
+        this(defaultTranslation, miwokTranslation, NO_IMAGE_PROVIDED);
+    }
+
+    /**
+     * Constructs a new Word with initial values of the default language, Miwok language, and the
+     * image resource ID
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     * @param miwokTranslation is the word in the Miwok language
+     * @param imageResourceId is the drawable resource ID for the image associated with the word
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        this.mDefaultTranslation = defaultTranslation;
+        this.mMiwokTranslation = miwokTranslation;
+        this.mImageResourceId = imageResourceId;
     }
 
     /**
      * Get the default translation of the word
      * @return default translation of the word
      */
-    public String getDefaltTranslation() {
-        return defaltTranslation;
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
     }
 
     /**
      * Get the Miwok translation of the word
      * @return Miwok translation of the word
      */
-    public String getMiwokTranslation () {
-        return miwokTranslation;
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    /**
+     * Get the image resource ID of the word
+     * @return image resource ID of the word
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Checks whether there is an image for the word
+     * @return boolean checking whether there is an image for the word
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
